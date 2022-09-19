195ec79d0887cce2209798b339852baaa77adc9f
amadeok <43666702+amadeok@users.noreply.github.com>
2022-09-20 01:23:41 +0200

    format

--- VRChatTTS.py
+++ VRChatTTS.py
@@ -1,57 +1,26 @@
-import autopyBot, win32gui,  win32com.client,win32con, pyautogui, win32api
-path = r"imgs"
-p = autopyBot.autopy.autopy(path)
-import pygetwindow as gw
+import win32gui,  win32com.client,win32con, pyautogui, win32api
 from time import sleep, time
 from playsound import playsound
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = False
-# import pyttsx3
-# engine = pyttsx3.init()
-# #engine.say("I will speak this text")
-# voices = engine.getProperty("voices")
-# print(voices)
-# engine.setProperty("voice", voices[3].id)
-# engine.save_to_file("faster text to speech", 'test.mp3')
-
-# engine.runAndWait()
-# sleep(1)
-# playsound("test.mp3") 
-
-#ret = p.find([p.i.play], loop=2, timeout=10, region=[2560, 200, 500, 200])
-#p.mouse_move(ret.found, 0, 0)
-
-#from gtts import gTTS 
-# myobj = gTTS(text="mytext", lang='en-uk', slow=False)
-# myobj.save("welcome.mp3")
-# playsound("welcome.mp3") 
 
 def callback(hwnd, helper):
 
-    rect = win32gui.GetWindowRect(hwnd)
-    x = rect[0]
-    y = rect[1]
-    w = rect[2] - x
-    h = rect[3] - y
     winname = win32gui.GetWindowText(hwnd)
     if helper.win_name in winname and not "Visual" in winname:
         if not helper.use_balabolka or len(winname) > len(helper.win_name):
-            print("Window %s:" % win32gui.GetWindowText(hwnd))
-            print("\tLocation: (%d, %d)" % (x, y))
-            print("\t    Size: (%d, %d)" % (w, h))
+            print("Balabolka Window found: %s " % win32gui.GetWindowText(hwnd))
+
             helper.driver_win_handle = hwnd
     elif "vrchat" == winname.lower():
-        print("Window %s:" % win32gui.GetWindowText(hwnd))
-        print("\tLocation: (%d, %d)" % (x, y))
-        print("\t    Size: (%d, %d)" % (w, h))
+        print("VRChat Window found: %s " % win32gui.GetWindowText(hwnd))
+
         helper.vr_chat_handle = hwnd
-    elif "VRCHAT" in  winname:# == "VRCHAT tts help":
-        print("Window %s:" % win32gui.GetWindowText(hwnd))
-        print("\tLocation: (%d, %d)" % (x, y))
-        print("\t    Size: (%d, %d)" % (w, h))
+    elif "VRCHAT" in  winname:
+        print("Window %s: " % win32gui.GetWindowText(hwnd))
+
         helper.this_win_handle = hwnd
-   # elif (len(winname)):
-   #\     print("Window %s:" % win32gui.GetWindowText(hwnd))
+
 
 res = pyautogui.size()
 small_win = [2560, 200, 600, 250]
@@ -62,7 +31,6 @@ class helper:
     def __init__(self) -> None:
         self.window_pos_x, self.window_pos_y, self.window_size_x,self.window_size_y = def_win
 
-        self.togglekey = "\\"
         self.use_immersive_reader = True
         self.background_mode_activated = False
         self.prev_log = ""
@@ -79,30 +47,20 @@ class helper:
 
         self.use_keylogger_for_input = False
         self.speak_task_queued = False
-       # self.word_win = gw.getWindowsWithTitle('Word')[0]
-        #self.word_win.activate()
+
         win32gui.EnumWindows(callback, self)
         shell = win32com.client.Dispatch("WScript.Shell")
         shell.SendKeys('%')
 
     def speak_with_word(self):
-        #self.word_win = gw.getWindowsWithTitle('Word')[0]
-        #self.word_win.activate()
         pyautogui.press('home')  
         pyautogui.keyDown('up') 
         pyautogui.keyDown('ctrl')  
-        #pyautogui.press('a')    
-       # pyautogui.press('backspace')    
-        # pyautogui.keyUp('ctrl')  
-        #pyautogui.keyDown('ctrl')  
         pyautogui.press('space')    
         pyautogui.keyUp('ctrl')  
-    # win32gui.SetForegroundWindow(self.vr_chat_handle)
-        pyautogui.click(50, 50)
 
     def speak_with_balabolka(self):
         pyautogui.press('f5')  
-        pyautogui.click(50, 50)
 
     def focus_speaker(self):
         rect = win32gui.GetWindowRect(self.driver_win_handle)
@@ -118,12 +76,6 @@ class helper:
         win32gui.SetForegroundWindow(self.vr_chat_handle)
 
 if __name__ == "__main__":
-    print("main")
-    # if you want a keylogger to send to your email
-    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
-    # if you want a keylogger to record keylogs to a local file 
-    # (and then send it using your favorite method)
-
     h = helper()
     try:
         win32gui.ShowWindow(h.driver_win_handle, win32con.SW_MAXIMIZE)
@@ -135,8 +87,6 @@ if __name__ == "__main__":
     while 1:
         sleep(0.01)
 
-        #if win32api.GetAsyncKeyState(ord("Q")) == -32767 :
-        #    break
         if win32api.GetAsyncKeyState(114) == -32767:
             h.status_active = 1 - h.status_active
             if h.status_active:
@@ -149,22 +99,18 @@ if __name__ == "__main__":
                     except:
                         print("failed to get balabolka window, is it running?")
                     print(e)
-                #T = time()    
-                #if not h.use_balabolka:
                 pyautogui.keyDown('ctrl')  
                 pyautogui.press('a')    
                 pyautogui.keyUp('ctrl')
                 pyautogui.press('backspace')    
                 playsound('beepwav.wav')
 
-                #print(time() - T)
 
             else:
                 if h.use_balabolka:
                     h.speak_with_balabolka()
                 else:
                     h.speak_with_word()
-                #sleep(0.05)
                 try:
                     h.focus_vrchat()
                 except Exception as e:
